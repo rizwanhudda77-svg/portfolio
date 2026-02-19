f37a5be546af9f34d0a59ea1606c60804289d65b
rizwanhudda77-svg <rizwanhudda77@gmail.com>
2026-02-19 14:19:29 +0000

    update

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.nav');
+const toggle = document.querySelector('.nav__toggle');
+const links = document.querySelectorAll('.nav__menu a');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('is-open');
+
+  const isOpen = nav.classList.contains('is-open');
+  toggle.setAttribute('aria-expanded', isOpen);
+});
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('is-open');
+    toggle.setAttribute('aria-expanded', false);
+  });
+});
